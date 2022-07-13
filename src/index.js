551e81ddafd10366edbbe3f38cc292c09895d410
Kovendan Ragupathi <kovendankumar@gmail.com>
2022-07-13 19:00:46 +0530

    Props section started from 03 React Basics Tutorial

--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const title = 'World’s Greatest Classics (Box Set of 4 Book)';
+const author = 'Jane Austen F. Scott Fitzgerald, Oscar Wilde, Emily Brontë';
+const img =
+  'https://m.media-amazon.com/images/I/81j8i9slKIL._AC_UY327_FMwebp_QL65_.jpg';
+function BookList() {
+  return (
+    <section className="booklist">
+      <Book job="developer" />
+      <Book title="random title" number={22} />
+    </section>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Book = (props) => {
+  return (
+    <article className="Book">
+      <img src={img} alt="World's greatest Classics" />
+      <h1>{title}</h1>
+      <h4>{author.toUpperCase()}</h4>
+      <p>{props.job}</p>
+      <p>{props.title}</p>
+      <p>{props.number}</p>
+    </article>
+  );
+};
+
+// const Image = () => (
+//   <img
+//     src="https://m.media-amazon.com/images/I/81j8i9slKIL._AC_UY327_FMwebp_QL65_.jpg"
+//     alt="World's greatest Classics"
+//   />
+// );
+
+// const Title = () => <h1>World’s Greatest Classics (Box Set of 4 Books)</h1>;
+// const Author = () => (
+//   <h4 style={{ color: '#617d98', fontSize: '0.75rem', marginTop: '0.25rem' }}>
+//     Jane Austen F. Scott Fitzgerald, Oscar Wilde, Emily Brontë
+//   </h4>
+// );
+
+ReactDOM.render(<BookList />, document.getElementById('root'));
+// export default Greeting;
